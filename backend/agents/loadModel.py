39217405777c2117a8ea3ceb2436c1a1d032ae14
Mark Lights <mlights3@gatech.edu>
2026-04-04 22:45:04 -0400

    chose wrong models

--- backend/agents/loadModel.py
+++ backend/agents/loadModel.py
@@ -9,18 +9,18 @@ load_dotenv()
 llm_small = None
 llm_large = None
 GROQ_MODEL_llama3_1 = None
-GROQ_MODEL_GUARD_22 = None
-GROQ_MODEL_GUARD_86 = None
+GROQ_MODEL_groq_compound-mini = None
+GROQ_MODEL_groq_compound = None
 GROQ_MODEL_llama3_3 = None
 if os.getenv("USE_LOCAL") == 'true':
     print('local model')
     GROQ_MODEL_llama3_1 = ChatOllama(model="llama3.2:3b", temperature=0, max_tokens=4096)
-    GROQ_MODEL_GUARD_22 = ChatOllama(model="llama3.2:3b", temperature=0, max_tokens=4096)
-    GROQ_MODEL_GUARD_86 = ChatOllama(model="llama3.2:3b", temperature=0, max_tokens=4096)
+    GROQ_MODEL_groq_compound-mini = ChatOllama(model="llama3.2:3b", temperature=0, max_tokens=4096)
+    GROQ_MODEL_groq_compound = ChatOllama(model="llama3.2:3b", temperature=0, max_tokens=4096)
     GROQ_MODEL_llama3_3 = ChatOllama(model="llama3.2:3b", temperature=0, max_tokens=4096)
 else:
     print('cloud model')
     GROQ_MODEL_llama3_1 = ChatGroq(model=os.getenv("GROQ_MODEL_llama3_1"), temperature=0, max_tokens=4096)
-    GROQ_MODEL_GUARD_22 = ChatGroq(model=os.getenv("GROQ_MODEL_GUARD_22"), temperature=0, max_tokens=4096)
-    GROQ_MODEL_GUARD_86 = ChatGroq(model=os.getenv("GROQ_MODEL_GUARD_86"), temperature=0, max_tokens=4096)
+    GROQ_MODEL_groq_compound-mini = ChatGroq(model=os.getenv("GROQ_MODEL_groq_compound-mini"), temperature=0, max_tokens=4096)
+    GROQ_MODEL_groq_compound = ChatGroq(model=os.getenv("GROQ_MODEL_groq_compound"), temperature=0, max_tokens=4096)
     GROQ_MODEL_llama3_3 = ChatGroq(model=os.getenv("GROQ_MODEL_llama3_3"), temperature=0, max_tokens=4096)
